a95206e28927dac724d70921e094c1afd0737aff
jothilakshmi-r <jothiragu90@gmail.com>
2020-05-18 17:29:13 +0530

    Jothi Updated her code

--- src/test/java/org/sample/GITPush.java
+++ src/test/java/org/sample/GITPush.java
@@ -2,7 +2,8 @@ package org.sample;
 
 public class GITPush {
 	private void main() {
-		System.out.println("Push your code GIT Hub");
+		System.out.println("Push your code  to GIT Hub");
+		System.out.println("Hi am downloaded this code from GIT Repo and now am pushing again into Repo");
 
 	}
 	
